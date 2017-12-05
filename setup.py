c52b45624b47271064e233acdaf8039df62da844
johncf <johncf@protonmail.com>
2017-12-05 15:05:56 +0530

    bump version to 1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ options = {
 
 setup(
     name = "Java Play",
-    version = "1.0",
+    version = "1.1",
     description = "A web-based Java Playground",
     executables = [Executable("server.py")],
     options = options)
